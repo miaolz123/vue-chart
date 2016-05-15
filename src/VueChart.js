dadecf6beb94a18d17b05c952f6ddea5fe36fad6
miaolz123 <miaolizhao@126.com>
2016-05-15 21:03:15 +0800

    version 0.0.1

--- src/VueChart.js
+++ src/VueChart.js
@@ -1,19 +1,48 @@
+import Chart from 'chart.js'
+
 export default {
-  template: '<div></div>',
+  template: '<canvas :width="width" :height="height"></canvas>',
   props: {
-    watches: {
-      type: Array,
-      default: () => ['source', 'show', 'toc'],
+    width: {
+      type: Number,
+      default: 400,
+    },
+    height: {
+      type: Number,
+      default: 250,
+    },
+    title: {
+      type: Object,
     },
   },
   data: () => ({
-    msg: 'hello', //test
+    defaultTitle: {
+      display: true,
+      text: 'My VueChart',
+    },
   }),
   ready() {
-    rende(this)
-    this.$watch('source', () => { rende(this) })
-    this.watches.forEach((v) => {
-      this.$watch(v, () => { rende(this) })
+    /* eslint-disable no-new */
+    const thisChart = new Chart(this.$el, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3]
+        }]
+      },
+      options: {
+        responsive: false,
+        title: Object.assign({}, this.defaultTitle, this.title),
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }],
+        },
+      },
     })
   },
 }
